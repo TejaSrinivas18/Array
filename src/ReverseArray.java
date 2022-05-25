bef3c8cd366bf6929a6ca691672d58b171d61a1a
TejaSrinivas18 <tejasrinivas19@gmail.com>
2022-05-25 22:28:20 +0530

    "first class"

---
+++ src/ReverseArray.java
@@ -0,0 +1,16 @@
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+				int arr[] = {1,2,3,4,5};
+				
+				//for (int i=0; i < arr.length; i++) {
+					//System.out.print(arr[arr.length-i-1]);
+				//}
+				for (int i = arr.length-1; i>=0 ; i--) {
+					System.out.print(arr[i]);
+				}
+			}
+	}
